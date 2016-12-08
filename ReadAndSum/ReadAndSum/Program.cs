6fbb9737a6e881a0cc5e8ce41b1e3d4e673a5874
Панкратова Татьяна <tate92@list.ru>
2016-12-08 17:34:44 +0400

    Сложение двух чисел

---
+++ ReadAndSum/ReadAndSum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int sum;
+            int.TryParse(Console.ReadLine(), out a);
+            int.TryParse(Console.ReadLine(), out b);
+            sum = a + b;
+            Console.WriteLine(a + " + " + b + " = " + sum);
+            Console.ReadKey();
+        }
+    }
+}
