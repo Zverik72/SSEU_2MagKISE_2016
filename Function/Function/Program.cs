f904519a9d52c9fab79fbfa7c44d2b1d85ef9cc4
Панкратова Татьяна <tate92@list.ru>
2016-12-08 20:36:59 +0400

    Лаб2_Задание2. Вычисление функции

---
+++ Function/Function/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double x, f;
+            a = 5; b = 12; c = 1992;
+            Console.Write("Введите значение X: ");
+            double.TryParse(Console.ReadLine(), out x);
+            f = (-x / a) * Math.Sqrt(Math.Pow(x, 2) + b * x) + c; // запись формулы с использованием математических функций класса Math
+            Console.WriteLine("Функция F(x) равна {0}",f);
+            Console.ReadKey();
+        }
+    }
+}
