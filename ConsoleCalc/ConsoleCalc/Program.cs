d60894af8953e6d3e21b999a1ce7c22d5b58456c
Панкратова Татьяна <tate92@list.ru>
2016-12-14 23:31:35 +0400

    Л2.П4 Консольный калькулятор: сначала вводим первый операнд, потом операцию, потом второй операнд

--- ConsoleCalc/ConsoleCalc/Program.cs
+++ ConsoleCalc/ConsoleCalc/Program.cs
@@ -10,22 +10,55 @@ namespace ConsoleCalc
     {
         static void Main(string[] args)
         {
-            double operand1, operand2, result=0;
+            double operand1, operand2=0, result=0;
             string operation;
-            if (double.TryParse(Console.ReadLine(), out operand1) && double.TryParse(Console.ReadLine(), out operand2))
-            {
+            Console.WriteLine("Введите число: ");
+                if (double.TryParse(Console.ReadLine(), out operand1))
+                {
+                    //выбираем операцию
+                    Console.WriteLine("Выберите действие: ");
+                    Console.WriteLine("+ - сложение");
+                    Console.WriteLine("- - вычитание");
+                    Console.WriteLine("* - умножение");
+                    Console.WriteLine("/ - деление");
+                    Console.WriteLine("sqrt - корень из числа");
+                    operation = Console.ReadLine();
+                    switch (operation)
+                    {
                 // выбираем операцию
                 operation = Console.ReadLine();
                 switch(operation)
                 {
-                    case "+":result = operand1 + operand2; break;
-                    case "-": result = operand1 - operand2; break;
-                    case "*": result = operand1 * operand2; break;
-                    case "/": result = operand1 / operand2; break;
+                    //операции с двумя операндами
+                     case "+":
+                     case "-":
+                     case "*":
+                     case "/":
+                         //считываем второй операнд
+                     Console.WriteLine("Введите второе значение: ");
+                if (double.TryParse(Console.ReadLine(), out operand2))
+                   {
+                     //какая именно операция?
+                    switch (operation)
+                     {
+                     case "+": result = operand1 + operand2; break;
+                     case "-": result = operand1 - operand2; break;
+                     case "*": result = operand1 * operand2; break;
+                     case "/": result = operand1 / operand2; break;
+                      };
+                    }
+                    else
+                   {
+                    Console.WriteLine("Операнд должен быть числом!");
+                    };
+                    break;
+                    //операция с одним операндом
                     case "sqrt": result = Math.Sqrt( operand1); break;
-                    default:Console.WriteLine("Неверная операция!"); break;
+                    //недопустимые операции
+                    default: Console.WriteLine("Неверная операция!"); break;
                 };
                 // выводим результат 
+                Console.WriteLine("Результат: ");
                 Console.WriteLine(operand1 + "" + operation + "" + operand2 + "=" + result);
             }
             else
