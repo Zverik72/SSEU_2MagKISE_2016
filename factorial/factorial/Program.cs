19ea17e836bb40f9fc651f0e989005a0268b1fed
Панкратова Татьяна <tate92@list.ru>
2016-12-15 02:38:11 +0400

    Лаб3.Задание1.Написать программу, вычисляющую факториал числа

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; //количество чисел
+            int i = 1;
+            int f = 1;
+            Console.WriteLine("Введите колличество чисел для вычисления факториала:");
+              if (int.TryParse(Console.ReadLine(), out n))
+              while (i < n + 1)
+              {
+               //факториал числа
+               f = f * i;
+               Console.WriteLine("{" + f + "}");
+               i++;
+                };
+               //вывод результата
+            Console.WriteLine("Факториал: " + f);
+               //завершение рограммы
+            Console.ReadKey();
+        }
+    }
+}
