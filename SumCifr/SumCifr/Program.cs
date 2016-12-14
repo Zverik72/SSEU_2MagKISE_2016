b91c7f3aad4135f2af1c5cd0f813e30f82b74af6
Панкратова Татьяна <tate92@list.ru>
2016-12-15 02:51:28 +0400

    Лаб3. Задание2. Написать программу, вычисляющую количество и сумму цифр введенного числа

---
+++ SumCifr/SumCifr/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumCifr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x=0, col = 0, sum = 0; // число, количество и сумма 
+            Console.Write("Введите число: ");
+            if (int.TryParse(Console.ReadLine(), out x))
+              {
+                do
+                    {
+                    sum += x % 10; 
+                    col++;
+                    x = x / 10;
+                      }
+                while (x / 10 != 0) ;
+                sum += x;
+                col++;
+                       }
+                else
+             {
+            Console.WriteLine("Введите целое число!");
+               };
+            Console.WriteLine("Cумма: " + sum);
+            Console.WriteLine("Количество: " + col);
+            Console.ReadKey();
+        }
+    }
+}
