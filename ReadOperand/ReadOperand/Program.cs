cbb7e1b2ca24d6f2263c8e3a7e8520f93ad272ae
Панкратова Татьяна <tate92@list.ru>
2016-12-16 00:46:56 +0400

    Л3.П4 Консольный калькулятор

---
+++ ReadOperand/ReadOperand/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOperand
+{
+    class Program
+    {
+        //ввод операнда с консоли с проверкой значения
+         static double ReadOperand()
+        {
+            double operand = 0; //значение операнда
+            Console.Write("Введите число: ");
+            //повторять ввод, пока не будет введено корректное значение
+            while 
+                (!double.TryParse(Console.ReadLine(), out operand))
+                {
+                  Console.Write("Неверное значение! Введите число: ");
+                  };
+            //вернуть значение
+            return operand;
+                    }
+         static void Main(string[] args)
+         {
+             //первый операнд
+             double operand1 = ReadOperand();
+             //второй операнд
+             double operand2;
+             //действие
+             string operation; 
+                 do
+             {
+                 //выбираем операцию
+                 operation = Console.ReadLine();
+                 operation = operation.ToLower();
+                 switch (operation)
+                 {
+                     //операция с двумя операндами
+                     case "+":
+                     case "-":
+                     case "*":
+                     case "/":
+                     //считывааем второй операнд
+                     operand2 = ReadOperand();
+                     //какая именно операция
+                     switch (operation)
+                     {
+                         case "+": operand1 = operand1 + operand2; break;
+                         case "-": operand1 = operand1 - operand2; break;
+                         case "*": operand1 = operand1* operand2; break;
+                         case "/": operand1 = operand1 / operand2; break;
+                     };
+                     break;
+                     //операции с одним операндом
+                     case "sqrt": operand1 = Math.Sqrt(operand1); break;
+                     //выход из программы
+                    case "quit": case "exit": return;
+                         //недопустимые операции
+                     default: Console.WriteLine("Неверная операция!"); break;
+                 };
+                     //выводим результат
+                     Console.WriteLine(" = " + operand1);
+                 }
+                 //пока не будет введена команда выхода
+                 while (!((operation == "quit") || (operation == "exit"))); 
+        }
+    }
+}
