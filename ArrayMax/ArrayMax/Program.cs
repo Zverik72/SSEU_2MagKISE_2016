4b1b94267252826a94c3c6c5d663591f391075ee
Панкратова Татьяна <tate92@list.ru>
2016-12-15 21:57:17 +0400

    Revert "Л3.П2 Поиск индексов всех максимальных значений в массиве"
    This reverts commit 36a77259d3bdfa5be010ed740162dab8d69a2abf.

--- ArrayMax/ArrayMax/Program.cs
+++ ArrayMax/ArrayMax/Program.cs
@@ -15,48 +15,36 @@ namespace ArrayMax
             do
             {
                 Console.Write("Введите длину массива: ");
-              Console.Write("Введите длину массива: ");
             }
             while
-            while 
             (!int.TryParse(Console.ReadLine(), out n) || (n <= 0));
             // инициализация массива 
             int[] a = new int[n];
+            // ввод значений массива 
             int i = 0;
             while (i < n)
             {
                 Console.Write("Введите a[" + i + "]:");
                 if (int.TryParse(Console.ReadLine(), out a[i] ))
-                Console.Write("Введите a[" + i + "]: ");
-                if (int.TryParse(Console.ReadLine(), out a[i]))
                 {
                     i++;
                 }
                 else
                 {
                     Console.WriteLine("Введите целое число!");
-                }; ;
-            };
-            //поиск максимального значения
-            int max = a[0];
-            string iMax = "0";
-            for (i = 1; i < a.Length; i++)
-            {
-               if (a[i] == max)
-                {
-                  iMax = iMax + ", " + i;
-                  }
-               else 
-               if (a[i] > max)
-                {
-                   max = a[i];
-                   iMax = i.ToString();
                 };
+                int max = a[0];
+                for (i=1;i<a.Length; i++)
+                {
+                    if (a[i] > max)
+                    {
+                        max = a[i];
+                    };
+                };
+                //
+                Console.WriteLine("Максимальное значение: " + max);
             };
             // выход 
-            //вывод результата
-            Console.WriteLine("Максимальное значение a[" + iMax + "] = " + max);
-            //выход из программы
             Console.ReadKey();
         }
     }
