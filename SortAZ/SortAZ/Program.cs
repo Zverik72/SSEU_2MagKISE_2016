b5f04f25b859dec6b0258cb9646ce1770b10f4a2
Панкратова Татьяна <tate92@list.ru>
2016-12-16 02:58:38 +0400

    Л4.П4 Сортировка слов по алфавиту

---
+++ SortAZ/SortAZ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ввод строки
+            Console.WriteLine("Введите слова, раделенные пробелами");
+            string words = Console.ReadLine();
+            // преобразование строки в массив, разделитель - пробел 
+            string[]wordsArr = words.Split(' ');
+            // сортировка массива
+            Array.Sort(wordsArr);
+            // собираем массив обратно в строку
+            words = wordsArr[0];
+            for (int i = 0; i < wordsArr.Length; ++i)
+                words = words + ' ' + wordsArr[i];
+            // вывод результата на экран
+            Console.WriteLine("Сортировка слов по алфавиту:");
+            Console.WriteLine(words);
+            Console.ReadKey();
+        }
+    }
+}
