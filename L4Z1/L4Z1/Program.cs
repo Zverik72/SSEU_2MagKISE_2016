f7ebaefd4642db7e3473db7e21beb0e22feec805
Панкратова Татьяна <tate92@list.ru>
2016-12-16 03:27:50 +0400

    Лаб4. Задание 1.
    Напишите программу, которая для любой нажатой клавиши выводит ее код в
    таблице Unicode

---
+++ L4Z1/L4Z1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4Z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            Console.WriteLine("Нажмите клавишу");
+            char.TryParse(Console.ReadLine(), out a);
+            int b;
+            b = Convert.ToInt32(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
