e382442fdf0ea2bf05bea03bf48f38df1fa82429
Панкратова Татьяна <tate92@list.ru>
2016-12-16 03:51:52 +0400

    Лаб 4. Задание 4.
    Реализуйте метод сжатия строки на основе счетчика повторяющихся
    символов.

---
+++ L4Z4/L4Z4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4Z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите слово для проверки: ");
+            string S = Console.ReadLine();
+            int x = 0;
+            char c = ' '; // предыдущий символ
+               foreach (char s in S) //задаем опорный символ
+                  {
+                    // проверяем символы
+                      foreach (char sa in S)
+                    //если не равен предыдущему и равен опорному
+                      if (sa != c && sa == s) 
+                    // увеличить на 1
+                      x++; 
+                    // если не равно 0
+                      if (x != 0) 
+                    Console.Write("{0}{1}", s, x); 
+                      x = 0;
+                      c = s; 
+                       }
+            Console.ReadKey();
+        }
+    }
+}
