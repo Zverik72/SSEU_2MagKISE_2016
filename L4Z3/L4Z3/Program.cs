6faca8d47b1b0d639519af565d94c367c6668bb2
Панкратова Татьяна <tate92@list.ru>
2016-12-16 03:43:37 +0400

    Лаб4. Задание 3
    Найти самое длинное слово

---
+++ L4Z3/L4Z3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4Z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод данных для проверки
+            Console.WriteLine("Введите фразу или предложение для проверки: ");
+            string S = Console.ReadLine();
+            S = S.Trim(); //удаляем пробелы в конце и начале строки (если они есть)
+            string[] a = S.Split(' ', '.', ',', '!', '?', ';', ':', '-'); //заносим в массив a все слова, разделённые символами . , ! ? ; : -) или пробелом
+            int max = 0;
+            int nomer = 0;
+             for (int i = 0; i < a.Length; i++)
+                  {
+                     if (a[i].Length > max)
+                        {
+                          max = a[i].Length;
+                          nomer = i;
+                          }
+                   }
+            Console.WriteLine("Самое длинное слово: " + a[nomer]);
+            Console.WriteLine("В самом длинном слове: " + max + " символов(а)");
+            Console.ReadKey();
+        }
+    }
+}
