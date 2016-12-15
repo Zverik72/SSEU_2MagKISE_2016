c540b2c72b2e2cf23676932bfc8bcd9a7ca90592
Панкратова Татьяна <tate92@list.ru>
2016-12-16 02:54:07 +0400

    Л4.П3 Преобразование строки в массив чисел

---
+++ Split/Split/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через ;");
+            string numbersStr = Console.ReadLine();
+                         //разбить строку по символу
+            string[] numbersStrArr = numbersStr.Split(';');
+                         //массив чисел той же длины, что и массив строк
+            double[] numbersArr = new double[numbersStrArr.Length];
+                         //для каждого элемента массива
+            int n = 0;
+            
+                         for (int i = 0; i < numbersStrArr.Length; i++)
+                             {
+                                 //преобразуем строки без пробелов в числа
+                                if (double.TryParse(numbersStrArr[i].Trim(), out numbersArr[n]))
+                                     {
+                                         n++;
+                                     };
+                             };
+                         //для каждого элемента массива
+                         for (int i = 0; i < numbersArr.Length; i++)
+                             {
+                                 //Вывести на экран
+                Console.WriteLine("число [" + i + "] = " + numbersArr[i]);
+                             };
+                         //временный массив длиной n
+            double[] buffer = new double[n];
+                         for (int i = 0; i < n; i++)
+                             {
+                                 //копируем в буфер
+                buffer[i] = numbersArr[i];
+                             };
+                         //копируем получившийся массив из буфера в numbersArr
+            numbersArr = buffer;
+            Console.ReadKey();
+        }
+    }
+}
