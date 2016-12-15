71d9bac25dd357e6ad4a83c9467f9aa07575b1fc
Панкратова Татьяна <tate92@list.ru>
2016-12-16 02:43:01 +0400

    Л4.П1 Фамилия, имя, отчество, если нет отчества

--- FIO/FIO/Program.cs
+++ FIO/FIO/Program.cs
@@ -17,26 +17,64 @@ namespace FIO
             Console.Write("Введите фамилию: ");
             string familiya = Console.ReadLine();
             //И.О.Фамилия
-            Console.WriteLine(imya[0] + ". " + otchestvo[0] + ". " + familiya);
-            Console.WriteLine(imya[0].ToString().ToUpper() + ". " //И.
-                              + otchestvo[0].ToString().ToUpper() + ". " //О.
-                              + familiya[0].ToString().ToUpper()   //Ф
-                              + familiya.Substring(1).ToLower()); //амилия
+            if (!string.IsNullOrEmpty(otchestvo))  //если отчество ввели
+            {
+           //   Console.WriteLine(imya[0] + ". " + otchestvo[0] + ". " + familiya);
+                Console.WriteLine(imya[0].ToString().ToUpper() + ". " //И.
+                                  + otchestvo[0].ToString().ToUpper() + ". " //О.
+                                  + familiya[0].ToString().ToUpper()   //Ф
+                                  + familiya.Substring(1).ToLower()); //амилия
+            }
+            else // иначе- отчество не ввели
+            {
+                // выводим без отчества
+                Console.WriteLine(imya[0].ToString().ToUpper() + ". " //И.
+                                  + familiya[0].ToString().ToUpper() //Ф
+                                  + familiya.Substring(1).ToLower()); //амилия
+            };
+
             //Фамилия И.О.
-            Console.WriteLine(familiya[0].ToString().ToUpper()  // Ф
+            if (!string.IsNullOrEmpty(otchestvo))//если отчество ввели
+            { 
+                Console.WriteLine(familiya[0].ToString().ToUpper()  // Ф
                               + familiya.Substring(1).ToLower() + " "  // амилия
                               + imya[0].ToString().ToUpper() + ". " //И.
                               + otchestvo[0].ToString().ToUpper());// О.
-            //Имя Отчество ФАМИЛИЯ
-            Console.WriteLine(imya[0].ToString().ToUpper() //И 
+             }
+            else
+            {
+                // Выводим без отчества
+                Console.WriteLine(familiya[0].ToString().ToUpper()
+                                  + familiya.Substring(1).ToLower() + " "
+                                  + imya[0].ToString().ToUpper() + ".");
+            };
+            if (!string.IsNullOrEmpty(otchestvo))//если отчество ввели
+                //Имя Отчество ФАМИЛИЯ
+            { 
+                Console.WriteLine(imya[0].ToString().ToUpper() //И 
                               + imya.Substring(1).ToLower() + " " //мя
                               + otchestvo[0].ToString().ToUpper() // О 
                               + otchestvo.Substring(1).ToLower() + " " //тчество
                               + familiya.ToUpper()); // ФАМИЛИЯ
-             //фам_и_о
-            Console.WriteLine((familiya.Substring(0, 3) + "_" //фам_
+             }
+            else
+            {
+                // Выводим без отчества
+                Console.WriteLine(imya[0].ToString().ToUpper()
+                                  + imya.Substring(1).ToLower() + " "
+                                  + familiya.ToUpper());
+             };
+            if (!string.IsNullOrEmpty(otchestvo))//если отчество ввели
+                //фам_и_о
+            { 
+                Console.WriteLine((familiya.Substring(0, 3) + "_" //фам_
                               + imya.Substring(0, 1) + "_" //и
                               + otchestvo.Substring(0, 1)).ToLower());//о
+             }
+            else
+            {
+                Console.WriteLine((familiya.Substring(0, 3) + "_" + imya.Substring(0, 1)));
+            };
             Console.ReadKey();
         }
     }
