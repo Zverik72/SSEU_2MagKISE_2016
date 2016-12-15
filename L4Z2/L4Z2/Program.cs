7498292ba00cab192b63d78ef656bcef299408c0
Панкратова Татьяна <tate92@list.ru>
2016-12-16 03:32:32 +0400

    Лаб4. Задание 2.
    Проверьте, является ли введенная строка палиндромом (перевертышем).

---
+++ L4Z2/L4Z2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово или фразу");
+            string str = Console.ReadLine();
+            str = str.ToLower();
+            string[] array = str.Split(' ');
+            string str2 = array[0];
+              for (int i = 1; i < array.Length; ++i)
+                 {
+                   str2 = str2 + array[i];
+                   }
+            int end = str2.Length;// последний символ в строке
+            int buf = str2.Length - 1;//последний индек массива
+            int col = str2.Length % 2;//для определения чет/нечет
+             switch (col)
+                  {
+                    case 0://если четное кол-во символов
+                    for (int i = 0; i < buf; ++i)
+                       {
+                         if (str2[i] == str2[buf])
+                             {
+                               buf--;
+                               if (i + 1 > buf)
+                               Console.WriteLine("Это палиндром!");
+                               }
+                               else
+                                   {
+                                     Console.WriteLine("Не является палиндромом ");
+                                     }
+                        }
+                              break;
+                   default:// если нечетное
+                   for (int i = 0; i != buf; ++i)
+                        {
+                          if (str2[i] == str2[buf])
+                              {
+                                buf--;
+                                if ((i + 1) == buf)
+                                Console.WriteLine("Это палиндром!");
+                                }
+                         else
+                              {
+                                Console.WriteLine("Не является палиндромом ");
+                               }
+                           }
+                         break;
+                      }
+            Console.ReadKey();
+        }
+    }
+}
